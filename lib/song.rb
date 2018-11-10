f6aa1671a65a1a92d245f75714bbebe2e1bd762d
Ashleigh Chun <ashleigh.chun@wework.com>
2018-11-10 19:24:03 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,25 +6,35 @@ class Song
   @@artist_count ={}
   @@genre_count = {}
  
- attr_accessor :artist
- attr_reader :genre
+ attr_accessor :song_hash, :artist, :genre
  
- #GENRES = []
  
-  def initialize(artist, genre)
-    @artist = artist
-    @genre = genre
+  def initialize(song_name)
+    @song_hash = {}
     @@count += 1 
+  end
+ 
+  def name
+    @name = name
+  end
+  
+  def add_song(song_name, artist, genre)
+    song_hash[song_name] ||= []
+    song_hash[song_name] << (artist, genre)
+  end
+  
+  def genre(genre)
+    song_hash[genre]
+  end
+  
+  def artist(artist)
+    song_hash[artist]
   end
  
   def self.count
     @@count
   end
   
-  def genre=(genre)
-    @genre = genre
-    @@genres << genre
-  end
 
   
 #binding.pry
