1c7c5df9a0f6cce23c4adf8c948e7e79ad4b3b08
Ashleigh Chun <ashleigh.chun@wework.com>
2018-11-10 20:18:42 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -53,5 +53,17 @@ class Song
     @@genre_count
   end 
   
-
+def self.artist_count
+    @@artist_count.each do |artist|
+      if @@artist_count[artist] 
+        @@artist_count[artist] += 1 
+      else 
+        @@artist_count[artist] = 1
+        
+      #binding.pry
+    end
+  
+  end
+    @@artist_count
+  end 
 end
