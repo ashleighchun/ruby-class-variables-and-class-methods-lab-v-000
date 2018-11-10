dda55e59687734a3744f7fe45a9a8eed220cc62b
Ashleigh Chun <ashleigh.chun@wework.com>
2018-11-10 20:12:46 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,8 +41,11 @@ class Song
   
   def self.genre_count
     @@genres.each do |genre|
-      @@genre_count[genre] = 1 
-
+      if @@genre_count[genre] 
+        @@genre_count[genre] += 1 
+      else 
+        @@genre_count[genre] = 1
+        
       binding.pry
     end
     
